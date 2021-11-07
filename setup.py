7be3669ff3cae7eaac27d7623dcaaa90e6c89422
chengsly <66853441+chengsly@users.noreply.github.com>
2021-11-07 10:20:20 -0500

    update setup.py
    edit1: change from 'dna2vec' to 'DeepMicrobeFinder'

--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='dna2vec',
+    name='DeepMicrobeFinder',
     version='1.0.0',
-    description='dna2vec: Consistent vector representations of variable-length k-mers',
-    author='Patrick Ng',
-    author_email='pn.appdev@gmail.com',
-    url='https://github.com/pnpnpn/dna2vec',
+    description='DeepMicrobeFinder',
+    author='Siliangyu Cheng, Shenwei Hou',
+    author_email='siliangc@usc.edu',
+    url='https://github.com/chengsly/DeepMicrobeFinder',
     license='MIT',
     install_requires=[
-        'gensim>=0.13,<1.0',
-        'logbook',
+        'tensorflow==1.15', 
+        'keras==2.2.4',
+        'numpy', 
+        'scipy==1.4.1',
+        'pandas', 
+        'sklearn', 
+        'biopython',
+        'h5py==2.10.0'
     ],
     packages=find_packages())
+'D
