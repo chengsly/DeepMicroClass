c03b882e9ec92c4eb0b2c45aece55cd08fd1ea97
Tianqi Tang <tianqit@usc.edu>
2022-11-24 20:30:44 -0800

    training script no hardcode

--- scripts/data_preprocessing/calculate_mash.sh
+++ scripts/data_preprocessing/calculate_mash.sh
@@ -20,16 +20,23 @@ dist_switch=0
 
 if [ sketch_switch ]; then
 
+    mash sketch -o $sketch_dir/pre20_archaea -p 16 -i $working_dir/clean_pre20/pre20_archaea.fa
+    mash sketch -o $sketch_dir/post20_archaea -p 16 -i $working_dir/clean_post20/post20_archaea.fa
 
-mash sketch -o $sketch_dir/pre20_archaea -p 16 -i $working_dir/clean_pre20/pre20_archaea.fa
-mash sketch -o $sketch_dir/post20_archaea -p 16 -i $working_dir/clean_post20/post20_archaea.fa
+    mash sketch -o $sketch_dir/pre20_bacteria -p 16 -i $working_dir/clean_pre20/pre20_bacteria.fa
+    mash sketch -o $sketch_dir/post20_bacteria -p 16 -i $working_dir/clean_post20/post20_bacteria.fa
 
-mash sketch -o $sketch_dir/pre20_bacteria -p 16 -i $working_dir/clean_pre20/pre20_bacteria.fa
-mash sketch -o $sketch_dir/post20_bacteria -p 16 -i $working_dir/clean_post20/post20_bacteria.fa
+    mash sketch -o $sketch_dir/pre20_eukaryote -p 16 -i $working_dir/clean_pre20/pre20_euk.fa
+    mash sketch -o $sketch_dir/post20_eukaryote -p 16 -i $working_dir/clean_post20/post20_euk.fa
 
-mash sketch -o $sketch_dir/pre20_eukaryote -p 16 -i $working_dir/clean_pre20/pre20_euk.fa
-mash sketch -o $sketch_dir/post20_eukaryote -p 16 -i $working_dir/clean_post20/post20_euk.fa
+    mash sketch -o $sketch_dir/euk_vir -p 16 -i $working_dir/vhdb_euk_vir.fa
 
-mash dist -p 16 $sketch_dir/pre20_archaea.msh $sketch_dir/post20_archaea.msh > $dist/archaea.tsv
-mash dist -p 16 $sketch_dir/pre20_bacteria.msh $sketch_dir/post20_bacteria.msh > $dist/bacteria.tsv
-mash dist -p 16 $sketch_dir/pre20_eukaryote.msh $sketch_dir/post20_eukaryote.msh > $dist/eukaryote.tsv
+fi
+
+if [ dist_switch ]; then
+
+    mash dist -p 16 $sketch_dir/pre20_archaea.msh $sketch_dir/post20_archaea.msh > $dist_dir/archaea.tsv
+    mash dist -p 16 $sketch_dir/pre20_bacteria.msh $sketch_dir/post20_bacteria.msh > $dist_dir/bacteria.tsv
+    mash dist -p 16 $sketch_dir/pre20_eukaryote.msh $sketch_dir/post20_eukaryote.msh > $dist_dir/eukaryote.tsv
+
+fi
--- training_pytorch.py
+++ training_pytorch.py
@@ -23,6 +23,12 @@ if torch.cuda.is_available():
     print(torch.cuda.get_device_name(0))
 else:
     device = torch.device("cpu")
+
+TRAINING_DATASET_CSV = 'data/train.csv'
+VAL_DATASET_CSV = 'data/val.csv'
+ONEHOT_PATH = 'data/single_onehot'
+BATCH_SIZE=256
+CHECKPOINT_DIR = 'data/pt_logs/checkpoint'
 ############################################################################################################################
 #                                             Training                                                                     #
 ############################################################################################################################
@@ -30,13 +36,14 @@ else:
 from sklearn.utils import class_weight
 import pandas as pd
 
-y = pd.read_table('data/train.csv', sep=',')
+
+y = pd.read_table(TRAINING_DATASET_CSV, sep=',')
 y = y['class']
 weight = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(y), y=y)
 training_weight = weight[y]
 training_weight = torch.from_numpy(training_weight).float().to(device)
 
-val_y = pd.read_table('data/test.csv', sep=',')
+val_y = pd.read_table(VAL_DATASET_CSV, sep=',')
 val_y = val_y['class']
 val_weight = weight[val_y]
 val_weight = torch.from_numpy(val_weight).float().to(device)
@@ -44,23 +51,23 @@ val_weight = torch.from_numpy(val_weight).float().to(device)
 weight = torch.tensor(weight, dtype=torch.float).to(device)
 print(weight)
 
-batch_size=256
-
 from DMF import LightningDMF
 
 model = DMF()
 
-from pytorch_lightning.callbacks import ModelCheckpoint, TQDMProgressBar
+from pytorch_lightning.callbacks import ModelCheckpoint
 trainer = pl.Trainer(
     accelerator='gpu',
     precision=16,
     max_epochs=1500,
     default_root_dir=f'data/pt_logs/',
     callbacks=[
-        ModelCheckpoint(dirpath=f'data/pt_logs/checkpoint_new/',
-        filename='{epoch}-{step}-{val_f1:.3f}-{val_acc:.3f}',
-        monitor='val_loss',
-        save_top_k=-1, mode='min', every_n_epochs=10, save_last=True)
+        ModelCheckpoint(
+            dirpath=CHECKPOINT_DIR,
+            filename='{epoch}-{step}-{val_f1:.3f}-{val_acc:.3f}',
+            monitor='val_loss',
+            save_top_k=-1, mode='min', every_n_epochs=10, save_last=True
+            )
         ],
     # benchmark=True,
     )
@@ -75,25 +82,25 @@ def custom_collate(batch):
     batch = list((utils.sample_onehot(sample[0], contig_len)[None, :, :], sample[1]) for sample in batch)
     return default_collate(batch)
 
-train_dataset = SequenceDataset('data/train.csv', 'data/single_onehot/', preload=False)
-val_dataset = SequenceDataset('data/test.csv', 'data/single_onehot/', preload=False)
+train_dataset = SequenceDataset(TRAINING_DATASET_CSV, ONEHOT_PATH, preload=False)
+val_dataset = SequenceDataset(VAL_DATASET_CSV, ONEHOT_PATH, preload=False)
 
 train_dataloaders = DataLoader(
     train_dataset,
-    batch_size=batch_size,
+    batch_size=BATCH_SIZE,
     shuffle=True,
     num_workers=16,
     collate_fn=custom_collate,
-    sampler=torch.utils.data.sampler.WeightedRandomSampler(training_weight, batch_size * 256)
+    sampler=torch.utils.data.sampler.WeightedRandomSampler(training_weight, BATCH_SIZE * 256)
 )
 
 val_dataloaders = DataLoader(
     val_dataset,
-    batch_size=batch_size,
+    batch_size=BATCH_SIZE,
     shuffle=True,
     num_workers=16,
     collate_fn=custom_collate,
-    sampler=torch.utils.data.sampler.WeightedRandomSampler(val_weight, batch_size * 32)
+    sampler=torch.utils.data.sampler.WeightedRandomSampler(val_weight, BATCH_SIZE * 32)
 )
 
 trainer.fit(
