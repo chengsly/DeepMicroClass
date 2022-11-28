62825122c33141e028d50bb654104d9b3b03c38a
Tianqi Tang <tianqit@usc.edu>
2022-11-28 14:32:29 -0800

    Random forest test

--- test_random_forest.py
+++ test_random_forest.py
@@ -1,9 +1,8 @@
 import numpy as np
-import constants
 import sklearn
 import optparse
-import utils
 import os, sys
+# import utils
 from sklearn import preprocessing
 from typing import Union
 import argparse
@@ -18,7 +17,9 @@ from pytorch_lightning import seed_everything, loggers
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.utils import class_weight
 import pandas as pd
+
 from SequenceData import SequenceDataset
+from DMF import KMerTransformer
 
 import wandb
 
@@ -58,7 +59,40 @@ val_dataloaders = DataLoader(
     # shuffle=True,
     num_workers=16,
     collate_fn=SequenceDataset.custom_collate,
-    sampler=torch.utils.data.sampler.WeightedRandomSampler(val_weight, BATCH_SIZE * 32)
+    sampler=torch.utils.data.sampler.WeightedRandomSampler(val_weight, BATCH_SIZE * 256)
 )
 
-model = RandomForestClassifier(n_estimators=100, max_depth=10, class_weight='balanced')
+# transformer = KMerTransformer(k=3, rearrange=True)
+
+train_X = []
+train_y = []
+for batch in train_dataloaders:
+    X = batch[0]
+    # X = transformer(X)
+    # X = torch.argmax(X, dim=1).long()
+    train_X.append(X.numpy())
+    train_y.append(batch[1].numpy())
+train_X = np.concatenate(train_X, axis=0)
+train_y = np.concatenate(train_y, axis=0)
+train_X = train_X.reshape(train_X.shape[0], -1)
+
+val_X = []
+val_y = []
+for batch in val_dataloaders:
+    X = batch[0]
+    # X = transformer(X)
+    # X = torch.argmax(X, dim=1).long()
+    val_X.append(X.numpy())
+    val_y.append(batch[1].numpy())
+val_X = np.concatenate(val_X, axis=0)
+val_y = np.concatenate(val_y, axis=0)
+val_X = val_X.reshape(val_X.shape[0], -1)
+
+model = RandomForestClassifier(n_estimators=100, max_depth=10, class_weight='balanced', n_jobs=16)
+
+model.fit(train_X, train_y)
+pred = model.predict(val_X)
+"""Calculate validation accuracy and F1 score, and log them to wandb"""
+val_acc = (pred == val_y).sum() / len(val_y)
+val_f1 = sklearn.metrics.f1_score(val_y, pred, average='weighted')
+wandb.log({'val_acc': val_acc, 'val_f1': val_f1})
