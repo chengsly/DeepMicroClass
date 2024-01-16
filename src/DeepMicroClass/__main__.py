b502cc12e6ace71e0f52c23d8c9c75089a89b20b
Tianqi Tang <tianqit@usc.edu>
2024-01-15 17:09:28 -0800

    Download latest model

--- src/DeepMicroClass/__main__.py
+++ src/DeepMicroClass/__main__.py
@@ -1,11 +1,32 @@
 import argparse
 from .predict import predict
 from importlib.resources import files
+import requests
 
+
+def download_file(url, destination):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(destination, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+    else:
+        raise Exception(f"Failed to download file: status code {response.status_code}")
+    
 
 def predict_wrapper(input, model, output_dir, encoding="one-hot", mode="hybrid", single_len=1000, device="cuda"):
     if not model:
-        model_path = files("DeepMicroClass").joinpath("model.ckpt")
+        url = "https://github.com/chengsly/DeepMicroClass/raw/master/src/DeepMicroClass/model.ckpt"
+        local_checkpoint_file = "model.ckpt"
+
+        # Download the file
+        print(f"Downloading model from {url}")
+        download_file(url, local_checkpoint_file)
+
+        model_path = local_checkpoint_file
+    else:
+        model_path = model
+
     predict(input, model_path, output_dir, encoding, mode, device)
 
 
