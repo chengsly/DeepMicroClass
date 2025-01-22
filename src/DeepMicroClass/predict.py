fddb8f9fd8bfb137d973bbcf9b7b5a91f916b247
TianXiaSummer <148132280+TianXiaSummer@users.noreply.github.com>
2025-01-22 21:42:46 +0800

    Add files via upload
    Normalize the prediction results of each query using the softmax function.
    
    A new column "best_choice" has been added to the output results, indicating the category with the highest predicted probability for each query:
    
    	1: Eukaryote
    	2: Eukaryote Virus
    	3: Plasmid
    	4: Prokaryote
    	5: Prokaryote Virus

--- src/DeepMicroClass/predict.py
+++ src/DeepMicroClass/predict.py
@@ -18,16 +18,13 @@ import pandas as pd
 import logging
 
 
-def predict(input_path, model_path, output_dir, encoding="one-hot", mode="hybrid", device="cuda", cpu_thread=0):
+def predict(input_path, model_path, output_dir, encoding="one-hot", mode="hybrid", device="cuda"):
     if torch.cuda.is_available():
         device = torch.device(device)
         print(f"Run DeepMicroClass with {device}.")
     elif not torch.cuda.is_available():
         print("GPU is not available on your device. Run DeepMicroClass with CPU.")
         device = torch.device("cpu")
-
-    if cpu_thread > 0:
-        torch.set_num_threads(cpu_thread)
 
     print("Step 1/3: Loading models from {}".format(model_path))
 
@@ -61,23 +58,24 @@ def predict(input_path, model_path, output_dir, encoding="one-hot", mode="hybrid
             n_percent = seq.upper().count("N") / len(seq)
             if n_percent > constants.N_THRESHOLD:
                 print(f"Skipping {record.id} due to too many Ns ({n_percent:.2f})")
-                score = [record.description] + [0] * 5
+                score = [record.description] + [0] * 5 + [0]
                 prediction_scores.append(score)
                 continue
             if len(seq) < constants.MIN_SEQ_LEN:
                 print(f"Skipping {record.id} due to too short length ({len(seq)})")
-                score = [record.description] + [0] * 5
+                score = [record.description] + [0] * 5 + [0]
                 prediction_scores.append(score)
                 continue
 
             if mode == "hybrid":
                 score = predict_hybrid(seq, model, model_cpu, encoding, prediction_scores, device=device)
-                prediction_scores.append([record.id] + score.tolist())
+                score_list = score.tolist()
+                prediction_scores.append([record.id] + score_list + [score_list.index(max(score_list))+1])
             elif mode == "single":
                 pass
     result_df = pd.DataFrame(
         prediction_scores,
-        columns=[constants.NAME, constants.EUK, constants.EUKVIR, constants.PLASMID, constants.PROK, constants.PROKVIR],
+        columns=[constants.NAME, constants.EUK, constants.EUKVIR, constants.PLASMID, constants.PROK, constants.PROKVIR, "best_choice"],
     )
     result_df.to_csv(result_file_path, sep="\t", index=False)
     print(f"Prediction result saved to {result_file_path}")
@@ -105,4 +103,14 @@ def predict_hybrid(seq, model, model_cpu, encoding, prediction_score, device="cu
         chunk_score = F.softmax(chunk_score, dim=1).cpu().numpy()
         chunk_score = chunk_score.sum(axis=0) * constants.SCORE_FACTOR[i]
         score += chunk_score
-    return score
+    
+    # return score
+
+    # min_value = np.min(score)
+    # max_value = np.max(score)
+    # normalized_score = (score - min_value) / (max_value - min_value)
+    # return normalized_score
+    
+    # Normalize the cumulative scores into probabilities using softmax
+    final_probabilities = F.softmax(torch.tensor(score),dim=0).numpy()
+    return final_probabilities
